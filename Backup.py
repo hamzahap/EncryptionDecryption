2fd31233c28a1341307b6b06b08a0b35e4382a5c
hamzahap <hamzahqatar123@gmail.com>
2022-05-05 10:36:19 -0230

    Update README and add main script

---
+++ Backup.py
@@ -0,0 +1,90 @@
+import pymysql
+import pymysql.cursors
+import pandas as pd
+from datetime import date
+from cryptography.fernet import Fernet
+import os
+
+DB_NAME = "UrDBName"
+TB_NAME = "UrTableName"
+ANOTHER_TB_NAME = "UrAnotherTableName"
+PASS = "UrPassword"
+USER = "UrUserName"
+HOST = "UrHost"
+
+def main(): 
+    #Make Directories if they don't exist
+    os.makedirs('Backups', exist_ok=True)
+    os.makedirs('Keys', exist_ok=True)
+    today = date.today()
+    datetoday = today.strftime("%b-%d-%Y")
+    #Make connection to database
+    connection = pymysql.connect(host= HOST,
+                            user= USER,
+                            password= PASS,
+                            database= DB_NAME,
+                            cursorclass=pymysql.cursors.DictCursor)
+    #Retrieve all the tables in Dictionary format
+    with connection:
+        result1 = sql_select(connection, TB_NAME)
+        result2 = sql_select(connection, ANOTHER_TB_NAME)
+    #Convert to pandas dataframe and save to csv
+    #Retrieve file name for further operations
+    table1 = make_backup(result1, datetoday, TB_NAME)
+    table2 = make_backup(result2, datetoday, ANOTHER_TB_NAME)
+    #Make key for today's date (When the script is ran)
+    write_key(datetoday)
+    key = load_key(datetoday)
+    #Encrypt the tables with the keys if you want
+    #encrypt(table1, key)
+    #encrypt(table2, key)
+
+    
+    
+def sql_select(connection, tablename):
+    #Retrieve data from table in database
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM {}".format(tablename)
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
+    return result
+
+def make_backup(result, datetoday, filename):
+    #Create Backups for each table
+    df = pd.DataFrame.from_dict(result)
+    df.to_csv('Backups/{}-{}.csv'.format(filename, datetoday))
+    print("{} csv file for {} created".format(filename, datetoday))
+    return 'Backups/{}-{}.csv'.format(filename, datetoday)
+
+def write_key(datetoday):
+    #Generate key
+    key = Fernet.generate_key()
+    with open("Keys/key-{}.key".format(datetoday), "wb") as key_file:
+        key_file.write(key)
+
+def load_key(datetoday):
+    #Load key
+    return open("Keys/key-{}.key".format(datetoday), "rb").read()
+
+def encrypt(filename, key):
+    #Encrypt file with given key
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        file_data = file.read()
+        encrypted_data = f.encrypt(file_data)
+        with open(filename, "wb") as file:
+            file.write(encrypted_data)
+    print("Encryption for {} done!".format(filename))
+    
+def decrypt(filename, key):
+    #Decrypt file method added for use if needed
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        encrypted_data = file.read()
+    decrypted_data = f.decrypt(encrypted_data)
+    with open(filename, "wb") as file:
+        file.write(decrypted_data)
+    print("Decryption for {} done!".format(filename))
+        
+main()
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # EncryptionDecryption
-Smol script using Fernet
+
+A simple Encryption/Decryption script for Databases using PyMySQL and Cryptography library. 
+
+# Backup.py
+
+The main function creates the Backups and Keys Directories if they don't exist. <br/>
+The encrypt and decrypt function do exactly what they sound like they do. <br/>
+The library used to make a connection with the database is https://pypi.org/project/PyMySQL/ <br/>
+The scripts collects data from tables in your database. <br/>
+The data is then saved into .csv files using pandas dataframes. <br/>
+The .csv files are then encrypted via the the cryptography Fernet library. <br/>
+
+To run the script type py Backup.py in the command line obviously. <br/>
+
+The file format for the csv files is Tablename-date.csv . <br/>
+The file format for the key files is Key-date.key . <br/>
+
+# Encrypt.py
+
+The main function looks for the first two arguments, the file and the key file. <br/>
+
+To run the script type py Encrypt.py "Tablename-date" "Key-date" in the command line. <br/>
+
+Note: The script has to be run in the same directory as the Backups and Keys folder access are manually set in the script.
+
+# Decrypt.py
+
+The main function looks for the first two arguments, the encrypted file and the key file. <br/>
+
+To run the script type py Decrypt.py "Tablename-date" "Key-date" in the command line. <br/>
+Note: The script has to be run in the same directory as the Backups and Keys folder access are manually set in the script.
